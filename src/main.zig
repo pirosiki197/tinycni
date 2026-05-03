aa92f251d7d6c78e4fc769fb950a312ba73b0139
pirosiki197 <lingmukangtai825@gmail.com>
2026-05-03 15:33:50 +0900

    refactor

--- src/main.zig
+++ src/main.zig
@@ -1,7 +1,7 @@
 const std = @import("std");
 const linux = std.os.linux;
 const log = std.log;
-const Environ = std.process.Environ;
+const Allocator = std.mem.Allocator;
 
 const tinycni = @import("tinycni");
 
@@ -19,9 +19,13 @@ pub fn main(init: std.process.Init) !void {
     const stdin = try stdin_reader.interface.allocRemaining(allocator, .unlimited);
     const input = try Input.parse(allocator, stdin, init.environ_map);
 
-    const gateway = try std.Io.net.Ip4Address.parse(input.config.gateway, 0);
-    const bridge_name = input.config.bridge;
+    switch (input.cmd) {
+        .add => try handleAdd(allocator, io, input),
+        .del => try handleDel(allocator, io, input),
+    }
+}
 
+fn handleAdd(allocator: Allocator, io: std.Io, input: Input) !void {
     var host_client = try tinycni.Client.init();
     defer host_client.deinit();
 
@@ -29,11 +33,12 @@ pub fn main(init: std.process.Init) !void {
     const netns_fd = linux.open(netns, .{}, 0);
     defer _ = linux.close(@intCast(netns_fd));
 
+    const bridge_name = input.config.bridge;
     const bridge_index = host_client.ifnameToIndex(bridge_name) catch blk: {
         try host_client.createBridge(bridge_name);
         const bridge_index = try host_client.ifnameToIndex(bridge_name);
         try host_client.upLink(bridge_index);
-        try host_client.addIpv4Addr(bridge_index, gateway.bytes);
+        try host_client.addIpv4Addr(bridge_index, input.config.gateway.bytes);
         try configureNat(io, input.config.subnet, "eth0");
         break :blk bridge_index;
     };
@@ -64,7 +69,13 @@ pub fn main(init: std.process.Init) !void {
 
     try netns_client.addIpv4Addr(netns_veth_index, [_]u8{ 10, 0, 0, 2 });
 
-    try netns_client.setDefaultGateway(netns_veth_index, gateway.bytes);
+    try netns_client.setDefaultGateway(netns_veth_index, input.config.gateway.bytes);
+}
+
+fn handleDel(allocator: Allocator, io: std.Io, input: Input) !void {
+    _ = allocator;
+    _ = io;
+    _ = input;
 }
 
 fn configureNat(io: std.Io, subnet: []const u8, out: []const u8) !void {
@@ -86,7 +97,7 @@ fn configureNat(io: std.Io, subnet: []const u8, out: []const u8) !void {
     _ = try proc.wait(io);
 }
 
-fn generateRandomVethName(allocator: std.mem.Allocator) ![]const u8 {
+fn generateRandomVethName(allocator: Allocator) ![]const u8 {
     const chars = "1234567890abcdefghijklmnopqrstuvvwxyz";
     const prefix = "veth_";
     const rand_len = 6;
@@ -113,14 +124,16 @@ const Input = struct {
 
     config: Config,
 
+    const Ip4Address = std.Io.net.Ip4Address;
+
     const Config = struct {
-        cniVersion: []const u8,
+        cni_version: []const u8,
         name: []const u8,
         type: []const u8,
 
         bridge: []const u8,
         subnet: []const u8,
-        gateway: []const u8,
+        gateway: Ip4Address,
     };
 
     const Command = enum {
@@ -138,10 +151,29 @@ const Input = struct {
         }
     };
 
-    fn parse(arena: std.mem.Allocator, stdin: []const u8, env: *const Environ.Map) !Input {
+    fn parse(arena: Allocator, stdin: []const u8, env: *const std.process.Environ.Map) !Input {
+        const StdinJson = struct {
+            cniVersion: []const u8,
+            name: []const u8,
+            type: []const u8,
+
+            bridge: []const u8,
+            subnet: []const u8,
+            gateway: []const u8,
+        };
+
         const err = error.InvalidInput;
 
-        const config = try std.json.parseFromSliceLeaky(Config, arena, stdin, .{});
+        const raw_config = try std.json.parseFromSliceLeaky(StdinJson, arena, stdin, .{});
+        const gateway = try Ip4Address.parse(raw_config.gateway, 0);
+        const config = Config{
+            .cni_version = raw_config.cniVersion,
+            .name = raw_config.name,
+            .type = raw_config.type,
+            .bridge = raw_config.bridge,
+            .subnet = raw_config.subnet,
+            .gateway = gateway,
+        };
 
         const cmd_str = env.get("CNI_COMMAND") orelse {
             log.err("missing CNI_COMMAND", .{});
